0b05fcef7fc13ea22eb6a7517a193837e00ae3da
Alex Contreras <alex.code29@gmail.com>
2023-04-15 18:51:36 -0500

    feat: useState to track what tab a user is on

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
 import Header from './conponents/Header';
 import Landing from './pages/Landing';
 
+import { useState } from 'react';
+
 function App() {
+  const [ currentTab, setCurrentTab ] = useState('Home');
+
+  function displayPage(tab) {
+    switch(tab) {
+      case 'Skills': 
+        // skill conponent
+        break;
+      case 'Projects': 
+        // project component
+        break;
+      case 'Contacts':
+        // contact me project
+        break;
+      default: 
+        return <Landing />
+    }
+  };
 
   return (
     <div className="App">
       <header className='header'>
-        <Header />
+        <Header setCurrentTab={setCurrentTab} currentTab={currentTab}/>
       </header>
 
       <section>
-        <Landing />
+        {displayPage(currentTab)}
       </section>
     </div>
   )
--- src/conponents/Header.jsx
+++ src/conponents/Header.jsx
@@ -1,10 +1,11 @@
-function Header() {
+function Header({ setCurrentTab, currentTab }) {
    return (
       <div className='page-tabs'>
          <ul>
-            <li>Skills</li>
-            <li>Projects</li>
-            <li>Contact Me</li>
+            <li onClick={() => setCurrentTab('Home')} className={currentTab === 'Home' ? 'active' : null}>HOME</li>
+            <li onClick={() => setCurrentTab('Skills')} className={currentTab === 'Skills' ? 'active' : null}>SKILLS</li>
+            <li onClick={() => setCurrentTab('Projects')} className={currentTab === 'Projects' ? 'active' : null}>PROJECTS</li>
+            <li onClick={() => setCurrentTab('Contacts')} className={currentTab === 'Contacts' ? 'active' : null}>CONTACTS</li>
          </ul>
       </div>
    )
