3df92adb7cdf39133aad735ab88a73c965fdcc44
Alex Contreras <alex.code29@gmail.com>
2023-04-15 15:17:43 -0500

    feat: header with links to website sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import Header from './conponents/Header';
+import Landing from './pages/Landing';
+
 function App() {
 
   return (
     <div className="App">
-      
+      <header>
+        <Header />
+      </header>
+
+      <section>
+        {/* The different page will go here    */}
+      </section>
     </div>
   )
 }
---
+++ src/conponents/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+   return (
+      <nav>
+         <ul>
+            <li>Skills</li>
+            <li>Projects</li>
+            <li>Contact Me</li>
+         </ul>
+      </nav>
+   )
+};
+
+export default Header;
