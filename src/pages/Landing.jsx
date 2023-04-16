c91e44719de236578165454e99b6884f8844e22e
Alex Contreras <alex.code29@gmail.com>
2023-04-16 10:22:53 -0500

    feat: user can download my resume and added font family to body

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,16 @@
+import Resume from '../assets/resume/Alex_Contreras_V2_Resume_Website.pdf'
+
 function Landing() {
    return (
-      <div className='titles'>
-         <h1 className='name'>Alex Contreras</h1>
-         <h2>Full-Stack Developer</h2>
+      <div>
+         <div className='titles'>
+            <h1 className='name'>Alex Contreras</h1>
+            <h2>Full-Stack Developer</h2>
+         </div>
+
+         <div className='landing-cta'>
+            <a href={Resume} download className='resume-download'>Download Resume</a>
+         </div>
       </div>
    )
 };
